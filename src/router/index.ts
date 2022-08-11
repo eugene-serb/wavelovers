936e4e419e1540d5f8eb99d7ed9d3e56d6a55899
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-08-11 18:14:18 +0300

    - added meta to routes. - deleted canonical in index template, cause this info now in routes meta.

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,26 +8,76 @@ const routes: Array<RouteRecordRaw> = [
         path: '/',
         name: 'patterns-view',
         component: PatternsView,
+        meta: {
+            title: 'Wavelovers',
+            metaTags: [],
+            linkTags: [
+                {
+                    rel: 'canonical',
+                    href: 'https://wavelovers.ru/',
+                },
+            ],
+        },
     },
     {
         path: '/custom',
         name: 'custom-view',
         component: () => import('@/views/CustomView.vue'),
+        meta: {
+            title: 'Wavelovers – Custom',
+            metaTags: [],
+            linkTags: [
+                {
+                    rel: 'canonical',
+                    href: 'https://wavelovers.ru/custom',
+                },
+            ],
+        },
     },
     {
         path: '/manual',
         name: 'manual-view',
         component: () => import('@/views/ManualView.vue'),
+        meta: {
+            title: 'Wavelovers – Manual',
+            metaTags: [],
+            linkTags: [
+                {
+                    rel: 'canonical',
+                    href: 'https://wavelovers.ru/manual',
+                },
+            ],
+        },
     },
     {
         path: '/diagnostic',
         name: 'diagnostic-view',
         component: () => import('@/views/DiagnosticView.vue'),
+        meta: {
+            title: 'Wavelovers – Diagnostic',
+            metaTags: [],
+            linkTags: [
+                {
+                    rel: 'canonical',
+                    href: 'https://wavelovers.ru/diagnostic',
+                },
+            ],
+        },
     },
     {
         path: '/404',
         name: '404',
         component: () => import('@/views/NotFoundView.vue'),
+        meta: {
+            title: 'Wavelovers – Page not found',
+            metaTags: [],
+            linkTags: [
+                {
+                    rel: 'canonical',
+                    href: 'https://wavelovers.ru/404',
+                },
+            ],
+        },
     },
     {
         path: '/:catchAll(.*)*',
