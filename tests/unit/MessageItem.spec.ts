5d1f99ff23ead3c0ceb9b104b3c8d32a508f1a7f
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-12-18 12:10:37 +0300

    add jest unit testing

---
+++ tests/unit/MessageItem.spec.ts
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils';
+import MessageItem from '@/components/MessageItem.vue';
+
+describe('MessageItem.vue', () => {
+  it('renders slots when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(MessageItem, {
+      slots: { default: msg }
+    });
+    expect(wrapper.html()).toContain(msg);
+  });
+});
