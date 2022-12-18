8d5c58c17cdfddf62ee2563d9625cf2cb94dae77
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-12-18 12:44:45 +0300

    Update FooterItem unit test

---
+++ tests/unit/FooterItem.spec.ts
@@ -0,0 +1,9 @@
+﻿import { shallowMount } from '@vue/test-utils';
+import FooterItem from '@/components/FooterItem.vue';
+
+describe('FooterItem.vue', () => {
+  it('renders slots when passed', () => {
+    const wrapper = shallowMount(FooterItem);
+    expect(wrapper.html()).toContain('Wavelovers');
+  });
+});
