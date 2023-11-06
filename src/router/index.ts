f88435993134d3cf9025cda6df3893b4532289dc
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-11-06 17:32:22 +0300

    #36 update name and path of meta assets

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MetaTagUpdater from 'vue-meta-tag-updater';
 import routes from '@/router/assets/routes';
-import metas from '@/router/assets/metas';
+import meta from '@/router/assets/meta';
 
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
 
@@ -12,7 +12,7 @@ const router = createRouter({
 
 router.beforeEach(
   (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
-    MetaTagUpdater.update(to, from, next, metas);
+    MetaTagUpdater.update(to, from, next, meta);
   },
 );
 
