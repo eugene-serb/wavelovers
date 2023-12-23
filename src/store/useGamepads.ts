ebcc3bb6d4997bd8518446eb507237b08c89534e
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-12-23 17:53:51 +0300

    #49 add jsdoc for gamepad store

--- src/store/useGamepads.ts
+++ src/store/useGamepads.ts
@@ -1,15 +1,34 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
-import patterns from '@/assets/patterns.json';
 import { Vibrator } from '@/models';
+import patterns from '@/assets/patterns.json';
 
 import type { TVibrator } from '@/models';
 
+/**
+ * Стор геймпадов.
+ */
 export const useGamepads = defineStore('gamepads', () => {
+  /**
+   * Номер шаблона вибрации.
+   */
   const mode = ref<number>(0);
+
+  /**
+   * Активна вибрация?
+   */
   const isActive = ref<boolean>(false);
+
+  /**
+   * Геймпады.
+   */
   const gamepads = ref<TVibrator[]>([]);
 
+  /**
+   * Сменить вибрацию.
+   * 
+   * @param index Номер вибрации.
+   */
   function change(index: number): void {
     if (mode.value === index) {
       isActive.value = !isActive.value;
@@ -25,6 +44,11 @@ export const useGamepads = defineStore('gamepads', () => {
     }
   }
 
+  /**
+   * Добавить геймпад.
+   * 
+   * @param event Событие геймпада.
+   */
   function addGamepad(event: GamepadEvent): void {
     if (gamepads.value.length >= 1) {
       return;
@@ -33,18 +57,36 @@ export const useGamepads = defineStore('gamepads', () => {
     gamepads.value.push(new Vibrator(event.gamepad));
   }
 
+  /**
+   * Удалить геймпад.
+   * 
+   * @param event Событие геймпада.
+   */
   function deleteGamepad(event: GamepadEvent): void {
     gamepads.value = gamepads.value.filter((gamepad) => gamepad.device.id !== event.gamepad.id);
   }
 
+  /**
+   * Воспроизвести дорожку вибрации.
+   * 
+   * @param pattern Дорожка шаблонов вибраций.
+   */
   function loop(pattern: GamepadEffectParameters[]): void {
     gamepads.value.forEach((gamepad) => gamepad.loop(pattern));
   }
 
+  /**
+   * Запустить вибрацию.
+   * 
+   * @param pattern Шаблон вибрации.
+   */
   function vibrate(pattern: GamepadEffectParameters): void {
     gamepads.value.forEach((gamepad) => gamepad.vibrate(pattern));
   }
 
+  /**
+   * Отключить вибрацию.
+   */
   function reset(): void {
     gamepads.value.forEach((gamepad) => gamepad.reset());
   }
