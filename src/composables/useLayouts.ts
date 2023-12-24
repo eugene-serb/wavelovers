1826462cc57eca0a663eda7d61e23e7b06b8d600
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2023-12-24 10:12:33 +0300

    #53 add composable useLayouts

---
+++ src/composables/useLayouts.ts
@@ -0,0 +1,29 @@
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import LDefault from '@/layouts/LDefault.vue';
+import LGamepad from '@/layouts/LGamepad.vue';
+
+import type { Component } from 'vue';
+
+const layouts: { [index: string]: Component } = {
+  DEFAULT: LDefault,
+  GAMEPAD: LGamepad,
+};
+
+export function useLayouts() {
+  const route = useRoute();
+
+  const layout = computed<Component>(() => {
+    const routeLayout = route?.meta?.layout;
+
+    return routeLayout && typeof routeLayout === 'string'
+      ? layouts[routeLayout]
+      : layouts.DEFAULT;
+  });
+
+  return {
+    layout,
+  };
+}
+
+export default useLayouts;
