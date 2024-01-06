fe47b5f48a47e61faf9e4f78ed4e7c51d5b67ceb
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2024-01-06 19:41:40 +0300

    #69 add jsdoc

--- src/models/Router.ts
+++ src/models/Router.ts
@@ -1,14 +1,44 @@
+/**
+ * Аттрибуты тега.
+ */
 export interface ITag {
+  /**
+   * Аттрибут.
+   */
   [key: string]: string;
 }
 
+/**
+ * Мета информация страницы.
+ */
 export interface IMeta {
+  /**
+   * Заголовок страницы.
+   */
   title: string;
+
+  /**
+   * Мета теги meta.
+   */
   metaTags: Array<ITag>;
+
+  /**
+   * Мета теги link.
+   */
   linkTags: Array<ITag>;
 }
 
+/**
+ * Объект ассоциирующий мета информацию со страницей.
+ */
 export interface IRouteMeta {
+  /**
+   * Путь.
+   */
   path: string;
+
+  /**
+   * Мета информации страницы.
+   */
   meta: IMeta;
 }
