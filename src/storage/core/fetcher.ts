bc99dea3651563b6d76626f0de89a98222ec2ea5
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2024-02-02 19:31:49 +0300

    #102 type fetcher and add jsdoc

--- src/storage/core/fetcher.ts
+++ src/storage/core/fetcher.ts
@@ -1,23 +1,46 @@
-import type { CommonStorageModel } from './types';
+import type { CommonStorageModel, Fetcher } from './types';
 
+/**
+ * Извлечь данные.
+ *
+ * @param store Модель данных хранилища.
+ * @returns Данные.
+ */
 function get<T>(store: CommonStorageModel<T>): T {
   const dto = localStorage.getItem(store.id);
   return store.deserializer(dto);
 }
 
+/**
+ * Записать данные.
+ *
+ * @param data Данные.
+ * @param store Модель данных хранилища.
+ */
 function set<T>(data: T, store: CommonStorageModel<T>): void {
   localStorage.setItem(store.id, store.serializer(data));
 }
 
+/**
+ * Удалить данные.
+ *
+ * @param store Модель данных хранилища.
+ */
 function remove<T>(store: CommonStorageModel<T>): void {
   localStorage.removeItem(store.id);
 }
 
+/**
+ * Очистить всё хранилище.
+ */
 function clear(): void {
   localStorage.clear();
 }
 
-const fetcher = {
+/**
+ * Fetcher слоя storage.
+ */
+const fetcher: Fetcher = {
   get,
   set,
   remove,
