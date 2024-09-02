9d016ab52c0e0e3d017e8b75b421a20a933a427c
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2024-09-02 18:46:01 +0300

    #139 delete jsx support

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 const ROOT = resolve(__dirname, 'src/pages');
@@ -12,7 +11,7 @@ const OUTPUT_DIR = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
